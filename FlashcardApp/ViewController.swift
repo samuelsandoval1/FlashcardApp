c26e55b9f332698643f72fe9859be3bfee926a72
Samuel Sandoval <sjsandoval1000@gmail.com>
2021-03-06 12:58:12 -0800

    Completed lab2

--- FlashcardApp/ViewController.swift
+++ FlashcardApp/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -21,6 +20,15 @@ class ViewController: UIViewController {
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         frontLabel.isHidden = true
     }
+    func updateFlashcard(question: String, answer: String){
+        frontLabel.text = question
+        backLabel.text = answer
+    }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationController = segue.destination as! UINavigationController
+        let creationController = navigationController.topViewController as! CreationViewController
+        creationController.flashcardController = self
+    }
 }
 
